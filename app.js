26dfd166e78937e482d695eaeb127ff5ad466141
Jasmin Esper <jasmin.09-@live.com.ar>
2024-07-01 22:34:41 -0300

    segunda entrega

--- app.js
+++ app.js
@@ -1,16 +1,51 @@
 import express from "express";
+import { engine } from "express-handlebars";
+import http from "http";
+import { Server } from "socket.io";
 import productsRouter from "./src/routers/productsRouter.js";
 import cartsRouter from "./src/routers/cartsRouter.js";
+import viewsRouter from "./src/routers/views.router.js";
 
 const app = express();
 const PUERTO = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("./src/public"));
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+const httpServer = http.createServer(app);
 
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
+app.use("/", viewsRouter);
 
-app.listen(PUERTO, () => {
+httpServer.listen(PUERTO, () => {
   console.log(`Servidor escuchando en el puerto ${PUERTO}`);
+});
+
+const io = new Server(httpServer);
+
+import ProductManager from "./src/productManager.js";
+const productManager = new ProductManager("./data/product.json");
+
+io.on("connection", async (socket) => {
+  console.log("Un cliente se conectó");
+
+  socket.emit("productos", await productManager.getProducts());
+
+  socket.on("eliminarProducto", async (id) => {
+    await productManager.deleteProduct(id);
+
+    io.sockets.emit("productos", await productManager.getProducts());
+  });
+
+  socket.on("agregarProducto", async (producto) => {
+    await productManager.addProduct(producto);
+
+    io.sockets.emit("productos", await productManager.getProducts());
+  });
 });
